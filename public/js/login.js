14c76d1e50994ebc46e0f33bdc9dc72683abecde
Peter Zbin <xzbin@stuba.sk>
2022-05-02 18:49:57 +0200

    FIX: BASE URL FIX 2

--- public/js/login.js
+++ public/js/login.js
@@ -438,7 +438,7 @@ const CONTRACT_ABI = [
 const CHAIN = "mumbai"      // siet na ktorej je smart kontrakt
 
 //const BASE_URL = "http://localhost:4000"     //URL mojej api
-const BASE_URL = "http://167.71.3.36/:4000"
+const BASE_URL = "http://167.71.3.36"
 
 Moralis.start({serverUrl: SERVER_URL, appId: APP_ID});
 let web3;
